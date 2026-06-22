744390637f52625b4023dd99ef0541bc158a64a8
Sudhanshu Gaikwad <140872777+sudhanshugaikwad@users.noreply.github.com>
2026-06-22 15:16:09 +0530

    Time: 13 ms (89%), Space: 74.6 MB (34.39%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    const seen = new Set();
+    
+    for (let num of nums) {
+        if (seen.has(num)) {
+            return true;
+        }
+        seen.add(num);
+    }
+    
+    return false;
+};
